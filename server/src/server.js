b497f61600d4e5ccc702af5804de2bcbc973b8dd
AnasPunggawa <anaspunggawa8882@gmail.com>
2023-07-15 08:51:03 +0800

    updated project

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,11 @@ const PORT = process.env.PORT || 5000;
 // MIDDLEWARES
 app.use(
   cors({
-    origin: [process.env.CLIENT_ORIGIN_DEV, process.env.CLIENT_ORIGIN_PROD],
+    origin: [
+      process.env.CLIENT_ORIGIN_DEV,
+      process.env.CLIENT_ORIGIN_PROD,
+      process.env.CLIENT_ORIGIN_VERCEL,
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
